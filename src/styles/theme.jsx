c651831c09939cca2ea47b8eff526a15dc8f7bd1
bin <simsang7566@dsm.hs.kr>
2024-07-16 12:21:19 +0900

    💄::디자인 시스템 컬러 추가

---
+++ src/styles/theme.jsx
@@ -0,0 +1,25 @@
+export const theme= {
+   Blue: {
+       "main 500": "#5E94F7",
+       "main 400": "#5EB3F7",
+       "main 300": "#5ED1F7",
+       "main 200": "#5EF0F7",
+       "main 100": "#5EF7E0",
+   },
+   Gray: {
+       "gray 700": "#333333",
+       "gray 600": "#444444",
+       "gray 500": "#7F7F7F",
+       "gray 400": "#CCCCCC",
+       "gray 300": "#E5E5E5",
+       "gray 200": "#F7F7F7",
+       "gray 100": "#FAFAFA",
+   },
+   Red: {
+       "error 900": "#520F11",
+       "error 800": "#791519",
+       "error 700": "#A01C21",
+       "error 600": "#C72329",
+       "error 500": "#DD3C41",
+   }
+}
