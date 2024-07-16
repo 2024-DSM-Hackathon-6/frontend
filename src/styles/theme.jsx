f7542bb57575aa3e5a70f0f8d8b9ae018dc72265
lhyunkyu <lhyunkyu2008@gmail.com>
2024-07-16 19:27:39 +0900

    💄폰트에 대한 스타일들을 추가

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,25 +1,115 @@
-export const theme= {
-   Blue: {
-       "main 500": "#5E94F7",
-       "main 400": "#5EB3F7",
-       "main 300": "#5ED1F7",
-       "main 200": "#5EF0F7",
-       "main 100": "#5EF7E0",
-   },
-   Gray: {
-       "gray 700": "#333333",
-       "gray 600": "#444444",
-       "gray 500": "#7F7F7F",
-       "gray 400": "#CCCCCC",
-       "gray 300": "#E5E5E5",
-       "gray 200": "#F7F7F7",
-       "gray 100": "#FAFAFA",
-   },
-   Red: {
-       "error 900": "#520F11",
-       "error 800": "#791519",
-       "error 700": "#A01C21",
-       "error 600": "#C72329",
-       "error 500": "#DD3C41",
-   }
+export const theme = {
+    Blue: {
+        "main 500": "#5E94F7",
+        "main 400": "#5EB3F7",
+        "main 300": "#5ED1F7",
+        "main 200": "#5EF0F7",
+        "main 100": "#5EF7E0",
+    },
+    Gray: {
+        "gray 700": "#333333",
+        "gray 600": "#444444",
+        "gray 500": "#7F7F7F",
+        "gray 400": "#CCCCCC",
+        "gray 300": "#E5E5E5",
+        "gray 200": "#F7F7F7",
+        "gray 100": "#FAFAFA",
+    },
+    Red: {
+        "error 900": "#520F11",
+        "error 800": "#791519",
+        "error 700": "#A01C21",
+        "error 600": "#C72329",
+        "error 500": "#DD3C41",
+    },
+    
+    loginHeading: {
+        fontSize: "48px",
+        fontWeight: normal,
+    },
+    placeholder: {
+        fontSize: "16px",
+        fontWeight: normal,
+    },
+    caption: {
+        fontSize: "16px",
+        fontWeight: normal,
+    },
+    searchHeading: {
+        fontSize: "18px",
+        fontWeight: normal,
+    },
+    button: {
+        fontSize: "18px",
+        fontWeight: normal,
+    },
+    findButton: {
+        fontSize: "14px",
+        fontWeight: normal,
+    },
+    searchHeading: {
+        fontSize: "32px",
+        fontWeight: normal,
+    },
+    searchHeading2: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    searchPlaceholder: {
+        fontSize: "18px",
+        fontWeight: normal,
+    },
+    tileOwner: {
+        fontSize: "13px",
+        fontWeight: normal,
+    },
+    tileHeading: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    tileP: {
+        fontSize: "16px",
+        fontWeight: normal,
+    },
+    titleDay: {
+        fontSize: "15px",
+        fontWeight: normal,
+    },
+    menuButton: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    writeHeading: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    writeButton: {
+        fontSize: "14px",
+        fontWeight: normal,
+    },
+    titleSearchHeading: {
+        fontSize: "14px",
+        fontWeight: normal,
+    },
+    titleSearchPlaceHolder: {
+        fontSize: "13px",
+        fontWeight: normal,
+    },
+    popUpHeading: {
+        fontSize: "24px",
+        fontWeight: normal,
+    },
+    popUpPlaceholder: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    okButton: {
+        fontSize: "20px",
+        fontWeight: normal,
+    },
+    writeWriting: {
+        fontSize: "16px",
+        fontWeight: normal,
+    },
+
 }
